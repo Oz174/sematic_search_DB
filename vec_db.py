b66b8bc74705407d1b0556fde42685f59074d091
Oz174 <ahmedtarek1754@gmail.com>
2023-12-20 23:53:40 +0200

    fixed(I think)

--- vec_db.py
+++ vec_db.py
@@ -1,27 +1,33 @@
 import faiss
-import numpy as np 
+import numpy as np
 
 
 class VecDB:
-  def __init__(self,file_path=None,new_db=False):
-    self.index = faiss.read_index(file_path)
+    def __init__(self, file_path=None, new_db=False):
+        if file_path:
+            self.index = faiss.read_index(file_path)
+        else:
+            self.index = None
 
-  def insert_records(self,records:dict):
-    data = list(records.values())
-    data = data[:,1:]
-    np.random.seed(42)
-    Xt = np.random.rand(10000, 70).astype('float32')
-    d = 70  # Dimension of the vectors
-    nlist = 100  # Number of clusters
-    quantizer = faiss.IndexFlatL2(d)  # Flat index for clustering
+    def insert_records(self, records: list(dict)):
+        data = list()
+        for i in range(len(records)):
+            print("alo")
+            data.append(records[i].get("embed"))
 
-    # Create IndexIVFFlat
-    self.index = faiss.IndexIVFFlat(quantizer, d, nlist, faiss.METRIC_L2)
+        data = np.array(data).astype('float32')
+        np.random.seed(42)
+        Xt = np.random.rand(2500, 70).astype('float32')
+        d = 70  # Dimension of the vectors
+        nlist = 100  # Number of clusters
+        quantizer = faiss.IndexFlatL2(d)  # Flat index for clustering
 
-    self.index.train(Xt)
-    self.index.add(data)
+        # Create IndexIVFFlat
+        self.index = faiss.IndexIVFFlat(quantizer, d, nlist, faiss.METRIC_L2)
 
-  def retrieve(self, query , top_k):
-    dists , ids = self.index.search(query,top_k)
-    return ids[0]
-  
+        self.index.train(Xt)
+        self.index.add(data)
+
+    def retrieve(self, query, top_k):
+        dists, ids = self.index.search(query, top_k)
+        return ids[0]
