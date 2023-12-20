f771054e131b9713c4e838b97f417448cf2003dc
Ahmed Tarek (オリ ー オ) <83515041+Oz174@users.noreply.github.com>
2023-12-20 22:47:42 +0200

    Update vec_db.py

--- vec_db.py
+++ vec_db.py
@@ -1,10 +1,27 @@
 import faiss
 import numpy as np 
 
-class VecDB():
-  def __init__(file_path = None , new_db = False):
-    pass
-  def insert_records(records):
-    # where I initialize faiss index (from winner winner chicken dinner)
-    pass
-    
+
+class VecDB:
+  def __init__(self,file_path=None,new_db=False):
+    self.index = faiss.read_index(file_path)
+
+  def insert_records(self,records:dict):
+    data = list(records.values())
+    data = data[:,1:]
+    np.random.seed(42)
+    Xt = np.random.rand(10000, 70).astype('float32')
+    d = 70  # Dimension of the vectors
+    nlist = 100  # Number of clusters
+    quantizer = faiss.IndexFlatL2(d)  # Flat index for clustering
+
+    # Create IndexIVFFlat
+    self.index = faiss.IndexIVFFlat(quantizer, d, nlist, faiss.METRIC_L2)
+
+    self.index.train(Xt)
+    self.index.add(data)
+
+  def retrieve(self, query , top_k):
+    dists , ids = self.index.search(query,top_k)
+    return ids[0]
+  
