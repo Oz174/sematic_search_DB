55cec5fc53c0cf0019fe0e6c652856ffa7030c00
Ahmed Tarek (オリ ー オ) <83515041+Oz174@users.noreply.github.com>
2023-12-20 22:14:58 +0200

    Create vec_db.py

---
+++ vec_db.py
@@ -0,0 +1,10 @@
+import faiss
+import numpy as np 
+
+class VecDB():
+  def __init__(file_path = None , new_db = False):
+    pass
+  def insert_records(records):
+    # where I initialize faiss index (from winner winner chicken dinner)
+    pass
+    
