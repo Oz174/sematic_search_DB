133daefcf4a3339d0f9ba23392c2eb8e626416b0
Ahmed Tarek (オリ ー オ) <83515041+Oz174@users.noreply.github.com>
2023-12-21 16:11:36 +0200

    Added seed to the training set + Changed to IVFPQ
    Better performance on 10K

--- vec_db.py
+++ vec_db.py
@@ -3,30 +3,59 @@ import numpy as np
 
 
 class VecDB:
-    def __init__(self, file_path=None, new_db=False):
-        if file_path:
-            self.index = faiss.read_index(file_path)
-        else:
-            self.index = None
+    def __init__(self, file_path=None, create_index=False):
+        self.index = None
 
-    def insert_records(self, records):
+        if file_path:
+            if create_index:
+                # Replace 128 with the dimension of your vectors
+                self.index = faiss.IndexFlatL2(70)
+            else:
+                self.index = faiss.read_index(file_path)
+
+    def add(self, raw_data, num_clusters):
+        if self.index is not None:
+            return
+        # Perform clustering using IVFPQ
+        # Assuming raw_data is a 2D array where each row is a vector
+        d = 70
+
         data = list()
-        for i in range(len(records)):
-            data.append(records[i].get("embed"))
+        for i in range(len(raw_data)):
+            data.append(raw_data[i].get("embed"))
 
         data = np.array(data).astype('float32')
-        np.random.seed(42)
-        Xt = np.random.rand(5000, 70).astype('float32')
-        d = 70  # Dimension of the vectors
-        nlist = 100  # Number of clusters
-        quantizer = faiss.IndexFlatL2(d)  # Flat index for clustering
 
-        # Create IndexIVFFlat
-        self.index = faiss.IndexIVFFlat(quantizer, d, nlist, faiss.METRIC_L2)
+        quantizer = faiss.IndexFlatL2(d)
+        # Adjust parameters as needed
+        index = faiss.IndexIVFPQ(quantizer, d, num_clusters, 35, 8)
 
-        self.index.train(Xt)
-        self.index.add(data)
+        # Train the index with the coarse quantizer
+        rng = np.random.default_rng(100)
+        Xt = rng.random((2500, 70), dtype=np.float32)
+        index.train(Xt)
 
-    def retrive(self, query, top_k):
-        _, ids = self.index.search(query, top_k)
-        return ids[0]
+        index.add(data)
+
+        # Save the index to the class member
+        self.index = index
+
+        return index
+
+    def search(self, query, top_k, metric='l2'):
+        # Perform search using the provided query
+        if self.index is None:
+            raise ValueError(
+                "Index not created. Use add() method to create the index.")
+
+        # Assuming query is a 2D array where each row is a vector
+        if metric == 'l2':
+            distances, indices = self.index.search(query, top_k)
+        elif metric == 'cosine':
+            # For cosine similarity, you may need to normalize the vectors
+            faiss.normalize_L2(query)
+            distances, indices = self.index.search(query, top_k)
+        else:
+            raise ValueError("Invalid distance metric. Use 'l2' or 'cosine'.")
+
+        return indices[0]
